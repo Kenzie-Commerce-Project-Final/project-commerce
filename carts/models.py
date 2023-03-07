917f0497f089e51a4b447627cc4113ea3c042667
Filipe Lucena <filipelucena1@gmail.com>
2023-03-07 11:49:35 -0300

    Core: creat all models and migrations

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Status(models.TextChoices):
+    REQUEST_MADE = "PEDIDO REALIZADO",
+    IN_PROGRESS = "EM ANDAMENTO",
+    DONE = "CONCLUÍDO"
+
+
+class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    items_count = models.IntegerField(default=0)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2)
+    status = models.CharField(max_length=50, choices=Status.choices, default=Status.REQUEST_MADE)
+    crated_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="carts",
+    )
