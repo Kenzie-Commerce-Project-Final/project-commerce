e39bc341929763f5f7948f514a69d7556e889c62
Silas Junior <silas.mcoach@gmail.com>
2023-03-07 14:56:58 -0300

    feat: partial finish code in serializer and view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import generics
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+
+class ProductView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
