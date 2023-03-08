889667c6ff22bc94a60f8257d27b36d91a59efc4
Silas Junior <silas.mcoach@gmail.com>
2023-03-08 14:26:05 -0300

    feat: process code in products

--- categories/views.py
+++ categories/views.py
@@ -2,8 +2,11 @@ from .models import Category
 from rest_framework import generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import CategorySerializer
+from .permissions import IsAdmPermissions
 
 
 class CategoryView(generics.ListCreateAPIView):
+    # permission_classes = [IsAdmPermissions]
+
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
