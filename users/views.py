f86340cbc13ce80bbc34b269725ed5612d8d9a1d
Kauan <kauancompositor@outlook.com>
2023-03-08 12:00:35 -0300

    feat: adding user creation and login

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import User
 from .serializers import UserSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 # Create your views here.
 
