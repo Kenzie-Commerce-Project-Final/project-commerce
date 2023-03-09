de424ce3b0f546b8750cf7063efb294f21d0be6f
Kauan <kauancompositor@outlook.com>
2023-03-09 12:35:23 -0300

    feat: adding route that lists products by id and route that filters user

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from . import views
 from rest_framework_simplejwt import views as jwt_views
+from users import views as users_views
 
 urlpatterns = [
-    path("", views.UserView.as_view()),
+    path("", users_views.UserView().as_view()),
     path("login/", jwt_views.TokenObtainPairView.as_view()),
+    path("<str:pk>/", users_views.UserDetailView.as_view()),
+    path("<str:pk>/products/", users_views.SellerProductsView().as_view()),
 ]
