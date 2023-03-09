5f474663e39f47a16cd2daeab65eee87c37afb6f
Silas Junior <silas.mcoach@gmail.com>
2023-03-09 12:42:31 -0300

    feat: add settings datetime in tokens

--- _kenzie_commerce/settings.py
+++ _kenzie_commerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 from django.core.management.utils import get_random_secret_key
 import dotenv
 import os
@@ -138,6 +139,11 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=24),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+}
 
 
 # Internationalization
