6584cb63c4e0207bad28935709f6d2ad71a4954f
Silas Junior <silas.mcoach@gmail.com>
2023-03-08 21:19:47 -0300

    feat: editing permissions in delete and patch

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.ProductView.as_view()),
     path("<id>/", views.ProductDetailView.as_view()),
+    path("<deletePatch_id>/", views.ProductDetailUpdateDestroyView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework import generics
-from .permissions import MyCustomPermissions
+from .permissions import MyCustomPermissions, IsPermissionPatchDelete
 
 
 class ProductView(generics.ListCreateAPIView):
@@ -28,9 +28,17 @@ class ProductView(generics.ListCreateAPIView):
         return serializer.save(user=self.request.user)
 
 
-class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+class ProductDetailView(generics.RetrieveAPIView):
     permission_classes = [MyCustomPermissions]
 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_url_kwarg = "id"
+
+
+class ProductDetailUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = [MyCustomPermissions]
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_url_kwarg = "deletePatch_id"
