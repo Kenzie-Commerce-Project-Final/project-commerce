b6ba11b4cb54df20ab67b9df91095a72d909d805
Silas Junior <silas.mcoach@gmail.com>
2023-03-13 12:51:41 -0300

    feat: edit setting page

--- _kenzie_commerce/settings.py
+++ _kenzie_commerce/settings.py
@@ -178,7 +178,7 @@ SPECTACULAR_SETTINGS = {
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 2,
+    "PAGE_SIZE": 8,
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
