5c8eb41a8849db0e381820b8cf7c1fdbc8a6b246
Silas Junior <silas.mcoach@gmail.com>
2023-03-13 12:14:24 -0300

    feat: atualization permission products list for Id

--- products/permissions.py
+++ products/permissions.py
@@ -14,6 +14,10 @@ class MyCustomPermissions(permissions.BasePermission):
 
 class IsOwnerOrSuperUser(permissions.BasePermission):
     def has_object_permission(self, request: Request, view: View, obj):
-        if request.user.is_superuser and request.user.is_authenticated:
+        if (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_superuser
+            and request.user.is_authenticated
+        ):
             return True
         return request.user.is_authenticated and request.user.id == obj.user.id
