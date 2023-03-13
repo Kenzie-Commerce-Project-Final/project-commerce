29ea8ea718052eef785719f0043746a7cbf56d12
Filipe Lucena <filipelucena1@gmail.com>
2023-03-13 12:41:02 -0300

    Feat: Add get users to admin accounts and create IsAdmToReadUsers permission

--- users/permissions.py
+++ users/permissions.py
@@ -7,7 +7,12 @@ class IsAdmOrUserCommon(permissions.BasePermission):
     def has_object_permission(self, request: Request, view: View, user: User):
         if request.user.is_superuser and request.user.is_authenticated:
             return True
-        import ipdb
-
-        # ipdb.set_trace()
         return request.user.is_authenticated and request.user.id == user.id
+
+
+class IsAdmToReadUsers(permissions.BasePermission):
+    def has_permission(self, request, view) -> bool:
+        if request.method in permissions.SAFE_METHODS:
+            if not request.user.is_superuser:
+                return False
+        return True
