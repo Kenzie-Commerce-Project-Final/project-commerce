2924d9f7478635e91d62eb07d8ba466c529ce1cc
Silas Junior <silas.mcoach@gmail.com>
2023-03-13 10:51:16 -0300

    feat: edit permissions products

--- categories/permissions.py
+++ categories/permissions.py
@@ -1,9 +1,9 @@
 from rest_framework import permissions
-from rest_framework.views import Request
+from rest_framework.views import Request, View
 
 
 class IsAdmPermissions(permissions.BasePermission):
-    def has_permission(self, request: Request):
+    def has_permission(self, request: Request, view: View):
         return (
             request.method in permissions.SAFE_METHODS
             or request.user.is_authenticated
--- categories/views.py
+++ categories/views.py
@@ -1,12 +1,11 @@
 from .models import Category
 from rest_framework import generics
-from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import CategorySerializer
-from rest_framework.permissions import IsAdminUser
+from .permissions import IsAdmPermissions
 
 
 class CategoryView(generics.ListCreateAPIView):
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAdmPermissions]
 
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
