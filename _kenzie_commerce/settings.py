8c7c0fad2fc17cbcd1f0ce5e074d958545104b23
Filipe Lucena <filipelucena1@gmail.com>
2023-03-13 15:16:13 -0300

    Fix: Fixing allowed_host configs on settings.py

--- _kenzie_commerce/settings.py
+++ _kenzie_commerce/settings.py
@@ -32,11 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False)
 
-ALLOWED_HOSTS = []
-RAILWAY_STATIC_URL = os.getenv("RAILWAY_STATIC_URL")
-
-if RAILWAY_STATIC_URL:
-    ALLOWED_HOSTS += [RAILWAY_STATIC_URL, "0.0.0.0"]
+ALLOWED_HOSTS = ["project-commerce-production.up.railway.app", "0.0.0.0"]
 
 # Application definition
 
